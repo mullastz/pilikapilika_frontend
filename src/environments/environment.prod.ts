b5bef1ca138d8885e9b587e0a5f131e90c6d25eb
sebakhan02 <sebaila360@gmail.com>
2026-04-16 00:48:32 +0300

    Remove src/environments from tracking

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://api.pilikapilika.com/api/v1',
-  appName: 'Pilika Pilika'
-};
--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://192.168.100.117:8000/api/v1',
-  appName: 'Pilika Pilika'
-};
